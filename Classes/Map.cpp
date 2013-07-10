4003d2509ecabf122f3ef40dbaf220da4b80d159
rhzchina <77620179@qq.com>
2013-07-11 03:52:19 +0800

    ad

--- Classes/Map.cpp
+++ Classes/Map.cpp
@@ -10,7 +10,7 @@ Map::Map(int level,GameScene* parent)
 	createData(level);
 	initMap(1);
 	curLevel = 1;*/
-	speed = 7.0f;
+	speed = 5.5f;
 	speedChange = 0;
 	map = CCArray::create();
 	map->retain();
